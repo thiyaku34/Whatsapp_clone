47b2367e0d04e2c4a554cfe31191d94c8594a882
Thiyagu <thiyakueali@gmail.com>
2026-03-16 22:50:43 +0530

    Fixed Python 3.14 Eventlet SSL issue and updated requirements

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request, redirect, session, jsonify
 from flask_socketio import SocketIO, emit
 import sqlite3
+import eventlet
+eventlet.monkey_patch()
 
 # -------------------------------
 # Flask + SocketIO setup
@@ -125,5 +127,6 @@ app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+    socketio.run(app, host="0.0.0.0", port=port, debug=False)
